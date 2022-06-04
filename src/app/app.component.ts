a058f0fd8cef00e9adcc4f9d7d0e4496a6d919b7
Paraisos05 <jeandelgado11@hotmail.com>
2022-06-04 04:22:12 -0600

    to1
    to2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { isWindow } from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  {
   title = 'front-ingSoft';
+  
+
+
+
+ 
 }
