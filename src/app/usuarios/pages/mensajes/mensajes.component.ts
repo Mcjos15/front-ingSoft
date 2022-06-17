1e9b90187b24ca7d75f0c1db56d6c3d0c0161781
Paraisos05 <jeandelgado11@hotmail.com>
2022-06-17 15:03:07 -0600

    el diablo
    xd

--- src/app/usuarios/pages/mensajes/mensajes.component.ts
+++ src/app/usuarios/pages/mensajes/mensajes.component.ts
@@ -1,10 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
+import { UserService } from '../../services/user.service';
+import {ActivatedRoute, Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { User } from '../../interfaces/users.interface';
+import { DataTableDirective } from 'angular-datatables';
 
 @Component({
   selector: 'app-mensajes',
   templateUrl: './mensajes.component.html',
-  styles: [
-  ]
+  styleUrls: ['./mensajes.component.css']
+  
 })
 export class MensajesComponent implements OnInit {
 
